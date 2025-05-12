81ff581e30f1eafba7047a6dd129b1599bde9d63
anusha7530 <agarwalanusha13@gmail.com>
2025-05-12 14:30:38 +0530

    Added components and styles

--- src/components/nested-comments.jsx
+++ src/components/nested-comments.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import { useState } from "react";
+import useCommentTree from "../hooks/useCommentTree";
+import Comment from "./comment";
+import "./styles.css";
 
-const NestedComments = () => {
+const NestedComments = ({
+  comments = [],
+  onSubmit = () => {},
+  onEdit = () => {},
+  onDelete = () => {},
+}) => {
+  const [comment, setComment] = useState("");
+
+  const {
+    comments: commentsData,
+    insertComment,
+    editComment,
+    deleteComment,
+  } = useCommentTree(comments);
+
+  const handleReply = (commentId, content) => {
+    insertComment(commentId, content);
+    onSubmit(content);
+  };
+
+  const handleEdit = (commentId, content) => {
+    editComment(commentId, content);
+    onEdit(content);
+  };
+
+  const handleDelete = (commentId) => {
+    deleteComment(commentId);
+    onDelete(commentId);
+  };
+
+  const handleEditChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (comment) {
+      handleReply(undefined, comment);
+      setComment("");
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="add-comment">
+        <textarea
+          value={comment}
+          onChange={handleEditChange}
+          rows={3}
+          cols={50}
+          className="comment-textarea"
+          placeholder="Add a new comment..."
+        />
+        <button onClick={handleSubmit} className="comment-button">
+          Add Comment
+        </button>
+      </div>
 
-export default NestedComments
+      {commentsData.map((comment) => (
+        <Comment
+          key={comment.id}
+          comment={comment}
+          onSubmitComment={handleReply}
+          onEditComment={handleEdit}
+          onDeleteComment={handleDelete}
+        />
+      ))}
+    </>
+  );
+};
+
+export default NestedComments;
